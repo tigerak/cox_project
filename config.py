fdd8cc4620b5d96cd64dde67846b0619413e5215
Youngbeom Heo <tigerakyb@gmail.com>
2025-04-19 21:52:49 +0900

    Add data_address

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+from pathlib import Path
+
 from secret import *
+
+CONFIG_PATH = Path(__file__).resolve()
+ROOT_DIR    = CONFIG_PATH.parent
+DB_PATH = str(ROOT_DIR / "data" / "chroma_db")
+DATA_PATH = str(ROOT_DIR / "data" / "final_result.pkl")
 
 OPENAI_MODEL_NAME = "gpt-4o-mini"
 OPENAI_EMBED_NAME = "text-embedding-3-small"
+
+HIDDEN_TAG = "<당신의 AI 상담사>"  # 화면에 숨길 구분자
+LOOKAHEAD  = len(HIDDEN_TAG)  # 태그 길이만큼 안전 버퍼
